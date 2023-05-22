15bae377b716c3ec76ad1b6150a2e89f8327f359
JulieGab <jullygabriella.sa@gmail.com>
2023-05-22 11:26:01 -0300

    aula 05

---
+++ modulo_02-JavaScript/aula_04_laco_for/exercicios.js
@@ -0,0 +1,50 @@
+//q01
+let i = 1;//1º passo
+while(i<=50){//2º passo
+    if(i%2!=0){//if(i%2==1)
+        console.log(i);
+    }
+    i=i+1;//3º passo
+}
+
+/*q01
+let i = 1;//1º passo
+while(i<=50){//2º passo
+    if(i%2!=0){//if(i%2==1)
+        console.log(i);
+    }
+    i=i+1;//3º passo
+}
+
+//q02
+let i = 1;
+let soma = 0;
+while(i<=5){
+    let numero = Number(prompt("Informe um número"))
+    soma = soma + numero;
+    i++;//i=i+1;3º passo
+}
+let media = soma/5;
+alert("Soma: "+soma+"\nMédia: "+media)*/
+
+//q03
+// let qtdePessoas = Number(prompt("Informe a qtde de pessoas: "))
+// let i = 1;//1º passo
+// let qtdeMenor21 = 0;
+// let qtdeMaior50 = 0;
+// while(i<=qtdePessoas){//2º passo
+//     let idade = Number(prompt("Informe a idade: "))
+//     if(idade<21){
+//         qtdeMenor21++;
+//     }
+//     if(idade>50){
+//         qtdeMaior50++;
+//     }
+//     i++;//3º passo
+// }
+// alert("Menor que 21: "+qtdeMenor21+"\nMaior que 50: "+qtdeMaior50)
+
+// //q04
+// for(let i = 1;i<=10;i++){
+//     console.log(i+" * 6 = "+(i*6));
+// }
---
+++ modulo_02-JavaScript/aula_05_function/conceito.js
@@ -0,0 +1,27 @@
+// // 4 operações básicas utilizando função
+
+// // subtracao
+// function subtracao(a, b){
+//     return a-b
+// }
+
+// let resultadoSubtracao = subtracao(10, 8)
+
+// console.log(resultadoSubtracao)
+
+// //divisao
+// function divisao(a, b){
+//     return a/b
+// }
+
+// let resultadoDivisao = divisao(24, 2)
+
+// console.log(resultadoDivisao)
+
+
+// function somar(n1,n2){ //2,3
+//     return n1+n2;
+// }
+
+// let res = somar(2,3) //chamada da função
+// console.log(res)
